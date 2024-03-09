abbcd4418b6fec87ffd4d502d81a0331c284aec7
Friedrich Vorländer <friedrich.vorlaender@icloud.com>
2024-03-09 11:03:20 +0100

    Initial Commit

---
+++ Project 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project 2
+//
+//  Created by Friedrich Vorländer on 09.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project 2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project 2
+//
+//  Created by Friedrich Vorländer on 09.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
